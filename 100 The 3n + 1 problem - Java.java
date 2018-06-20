d1a21309132c26ed1ecffc8f177420b6328be2db
GoulongWang <da60511@gmail.com>
2018-06-20 17:51:16 +0800

    Update and rename 100 The 3n + 1 problem - Java to 100 The 3n + 1 problem - Java.java

--- 100 The 3n + 1 problem - Java.java
+++ 100 The 3n + 1 problem - Java.java
@@ -6,16 +6,16 @@ class Main{
         while(input.hasNextInt()){
             int i = input.nextInt(), j = input.nextInt(); 
             System.out.print(i + " " + j + " ");
-	        if(i > j){
-	            int temp = i;
-	            i = j;
-	            j =temp;
-	        }
+	    if(i > j){
+	    	int temp = i;
+	        i = j;
+	        j =temp;
+	     }
 	    
-		    for (int a = i; a <= j; a++) if (length(a) >= max) max = length(a);
-		    System.out.println(max);
-		    max = 0;
-	    }
+	     for (int a = i; a <= j; a++) if (length(a) >= max) max = length(a);
+	     System.out.println(max);
+	     max = 0;
+	}
      }
      
      public static int length(int n){
