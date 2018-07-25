5583dd31dd429613dacdb009535d1008ddab0cf0
GuolongWang <da60511@gmail.com>
2018-07-25 21:36:24 +0800

    Update 579 Clock Hands.cpp

--- 579 Clock Hands.cpp
+++ 579 Clock Hands.cpp
@@ -7,10 +7,8 @@ int main(){
 
 	while(cin >> hour >> colon >> min && (hour || min)){
 		double HD = hour * 30 + min * 0.5, MD = min * 6;
-		if (HD >= MD) 
-			cout << setiosflags(ios::fixed) <<  setprecision(3) << (HD - MD <= 180 ? HD - MD: 360 - HD + MD) << endl;
-		else 
-			cout << setiosflags(ios::fixed) << setprecision(3) << (MD - HD <= 180 ? MD - HD: 360 - MD + HD) << endl;
+		if (HD >= MD) cout << setiosflags(ios::fixed) <<  setprecision(3) << (HD - MD <= 180 ? HD - MD: 360 - HD + MD) << endl;
+		else cout << setiosflags(ios::fixed) << setprecision(3) << (MD - HD <= 180 ? MD - HD: 360 - MD + HD) << endl;
 	}
 	
 	return 0;
