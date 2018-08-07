df736c1505ab01d508b5cee9933fcfafac8b9a82
GuolongWang <da60511@gmail.com>
2018-08-07 15:24:22 +0800

    Update 1225 Digit Counting.c

--- 1225 Digit Counting.c
+++ 1225 Digit Counting.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-void count(unsigned n, unsigned* FREQ){
-	while(n) FREQ[n % 10]++, n /= 10;
-}
-
 int main(){
 	unsigned T;
 	scanf("%u", &T);
 	while(T--){
 		unsigned N, freq[10] = {0}, i;
 		scanf("%u", &N);
-		for (i = N; i > 0; i--) count(i, freq);
+		for (i = N; i > 0; i--) {
+			int temp = i;
+			while(temp) freq[temp % 10]++, temp /= 10;
+		}
 		for (i = 0; i < 9; i++) printf("%u ", freq[i]); printf("%u\n", freq[9]);
 	}
 
