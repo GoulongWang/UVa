ddb5a3674bd7ed683f15c79bc5cc6fe028434c42
GuolongWang <da60511@gmail.com>
2018-08-07 15:24:59 +0800

    Update 1225 Digit Counting.cpp

--- 1225 Digit Counting.cpp
+++ 1225 Digit Counting.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
-void count(unsigned n, unsigned* FREQ){
-	while(n) FREQ[n % 10]++, n /= 10;
-}
-
 int main(){
 	unsigned T;
 	cin >> T;
 	while(T--){
 		unsigned N, freq[10] = {0}, i;
 		cin >> N;
-		for (i = N; i > 0; i--) count(i, freq);
-		for (i = 0; i < 9; i++) cout << freq[i] << " "; cout << freq[9] << endl;
+		for (i = N; i > 0; i--) {
+			int temp = i;
+			while(temp) freq[temp % 10]++, temp /= 10;
+		}
+		for (i = 0; i < 9; i++) cout << freq[i] << " ";
+		cout << freq[9] << endl;
 	}
 }
