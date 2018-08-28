38469770a6411f2fe01e3d3c3e01b500ba56fd17
GuolongWang <da60511@gmail.com>
2018-08-28 10:24:22 +0800

    Create 11547 Automatic Answer.cpp

---
+++ 11547 Automatic Answer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	int t, n;
+	cin >> t;
+	while(t--){
+		cin >> n;
+		cout << abs(315 * n + 36962) / 10 % 10 << endl;
+	}
+}
